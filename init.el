312cdaa70120a29601a19792be872336d71ae0d9
BETWEEN\srubio <salvador.rubio@between.tech>
2017-11-21 15:54:18 +0100

    update

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;;(setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
   ;; company is an optional dependency. You have to
@@ -33,3 +33,24 @@
 (add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme zenburn-theme tide which-key helm flycheck evil company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; flycheck
+;;(setq flycheck-check-syntax-automatically '(mode-enabled save new-line idle-change))
+(load-theme 'spacemacs-dark t)
+(set-face-attribute 'default nil :height 120)
