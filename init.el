d789dbad08c9ce928488a2d1c3a9c8471bb5b9ca
U-BETWEEN\srubio <srubio@MININT-582H8CB.between.loc>
2019-07-08 10:59:27 +0200

    no scroll bar msys2 gui

--- init.el
+++ init.el
@@ -187,7 +187,7 @@
 (setq inhibit-startup-screen t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-;;(scroll-bar-mode -1)
+(scroll-bar-mode -1)
 (electric-pair-mode 1)
 (global-auto-revert-mode t)
 (toggle-frame-maximized)
